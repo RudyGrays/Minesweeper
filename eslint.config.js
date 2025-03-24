0e7f7ca996f445e60744403c666d25dcd8d9c6f6
Rudy Grays <Egormatveev228@inbox.ru>
2025-03-25 00:45:05 +0500

    fix eslint problems

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,8 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "react-refresh/only-export-components": "off",
+      "react-hooks/exhaustive-deps": "off",
     },
   }
 );
